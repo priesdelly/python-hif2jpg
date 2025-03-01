db9347ca1023fb4d811712a12a8772f7b60c48e9
priesdelly <priesdelly@gmail.com>
2025-03-01 14:38:40 +0700

    feat: use effective log and validator and move to use .env settings

--- main.py
+++ main.py
@@ -1,27 +1,64 @@
 import os
 from concurrent.futures import ProcessPoolExecutor
 import logging
+from logging.handlers import QueueHandler, QueueListener
+from queue import Queue
 from typing import List, Tuple
 from pillow_heif import register_heif_opener
 from PIL import Image
-from tqdm import tqdm
+import colorlog
 
 # Register HEIF support
 register_heif_opener()
 
 # Configuration
-input_directory = "/Camera/event1-input"
-output_directory = "/Camera/event1-output"
-max_img_size = 4096
+input_directory = os.getenv("INPUT_DIRECTORY")
+output_directory = os.getenv("OUTPUT_DIRECTORY")
+max_img_size = int(os.getenv("MAX_IMG_SIZE"))
 
 
 # Setup logging
 def setup_logging():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        handlers=[logging.StreamHandler(), logging.FileHandler("conversion.log")],
+    log_queue = Queue()
+    queue_handler = QueueHandler(log_queue)
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            log_colors={
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red,bg_white",
+            },
+            datefmt="%Y-%m-%dT%H:%M:%S",
+        )
     )
+
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    root.addHandler(queue_handler)
+
+    listener = QueueListener(log_queue, handler)
+    listener.start()
+
+
+def validate_input() -> List[str]:
+    errors = []
+
+    if not input_directory:
+        errors.append("Input directory is not set")
+    if not output_directory:
+        errors.append("Output directory is not set")
+    if not max_img_size:
+        errors.append("Max image size is not set")
+    if not os.path.isdir(input_directory):
+        errors.append("Input directory does not exist")
+    if not os.path.isdir(output_directory):
+        errors.append("Output directory does not exist")
+
+    return errors
 
 
 def convert_and_resize(input_path: str, output_path: str) -> bool:
@@ -63,35 +100,47 @@ def process_file(file_input_path: str) -> Tuple[str, bool]:
 
 
 def main():
-
     setup_logging()
+    logging.info("Init conversion...")
+    logging.info("Validate input...")
+    errors = validate_input()
+    if errors:
+        for error in errors:
+            logging.error(error)
+        return
+
+    logging.info(f"Input directory: {input_directory}")
+    logging.info(f"Output directory: {output_directory}")
+    logging.info(f"Max image size: {max_img_size}")
 
     # Create output directory if it doesn't exist
     os.makedirs(output_directory, exist_ok=True)
 
     heif_files = get_heif_files(input_directory)
     heif_file_count = len(heif_files)
-
+    item_processed = 0
     # Use ProcessPoolExecutor for parallel processing
     with ProcessPoolExecutor() as executor:
-        with tqdm(total=heif_file_count) as pbar:
 
-            futures = {
-                executor.submit(process_file, file_input_path): file_input_path
-                for file_input_path in heif_files
-            }
+        futures = {
+            executor.submit(process_file, file_input_path): file_input_path
+            for file_input_path in heif_files
+        }
 
-            for future in futures:
-                file_name, success = future.result()
+        for future in futures:
+            file_name, success = future.result()
 
-                # Update progress bar description
-                pbar.set_description(f"Processing {file_name}")
+            item_processed += 1
+            percent = int(item_processed * 100 / heif_file_count)
 
-                # Update progress bar
-                pbar.update(1)
+            logging.info(
+                f"Processing ({item_processed}/{heif_file_count}) [{percent}%]: {file_name}"
+            )
 
-                if not success:
-                    logging.error(f"Failed to process {file_name}")
+            if not success:
+                logging.error(f"Failed to process {file_name}")
+
+    logging.info("Conversion completed")
 
 
 if __name__ == "__main__":
